e6695e8f63bb96a474021290ce237872ed5ca267
Nathan Brenton <48501813+ndbrenton@users.noreply.github.com>
2019-04-20 00:48:04 -0700

    Add files via upload
    Cat Clicker Premium.  Now dynamically generating the display area in a cleaner way

--- js/app.js
+++ js/app.js
@@ -1,35 +1,67 @@
-let body = document.querySelector('body');
-body.addEventListener('click', function() {
-    body.classList.toggle('menu-hidden');
-    console.log('Menu Hidden: ' + body.classList.contains('menu-hidden'));
-}, false);
+let display = document.querySelector('.display');
+let list = document.querySelector('.list');
 
-let pictures = [{ name: "Eyes",      src: "img/eyes.png"},
-                { name: "Meow",      src: "img/meow.png"},
-                { name: "Rocky",     src: "img/rocky.png"},
-                { name: "Sit",       src: "img/sit.png"},
-                { name: "Waterfall", src: "img/waterfall.png"}]; // console.log(pictures);
-
-var reformattedPictures = pictures.map(function(pic){
+// Array of Objects: Cat name and Cat picture URL
+let pictures = [{ name: "Eyes",      count: 0, src: "img/eyes.png"},
+                { name: "Meow",      count: 0, src: "img/meow.png"},
+                { name: "Rocky",     count: 0, src: "img/rocky.png"},
+                { name: "Sit",       count: 0, src: "img/sit.png"},
+                { name: "Waterfall", count: 0, src: "img/waterfall.png"}]; // console.log(pictures);
+// Array of Cat Names
+let pictureNames = pictures.map(function(pic){
     for(var i = 0; i < pictures.length; i++) {
         var rPic = {};
         rPic = pic.name;
         return rPic;
     }
-}); // console.log(reformattedPictures);
-
-let generatedFeedHTML = reformattedPictures.map ( i => '<li><a href="#" data-id="' + i + '">' + i + '</a></li>');
-let feed_list = document.querySelector('.feed-list');
-feed_list.innerHTML = generatedFeedHTML.join('');
+}); // console.log(pictureNames);
 
 
+// loop over the names in pictureNames array
+for (var i = 0; i < pictureNames.length; i++) {
+    // This is the name we're on...
+    var name = pictureNames[i];
+    var src = pictures[i].src;
+
+    // We're creating a DOM element for the name LIST
+    var elem = document.createElement('div');
+    elem.textContent = name; // add the name between the tags
+    elem.dataset.name = name; // add data property ... for maybe useful
+
+    // We're creating a DOM elements for the display div
+    var nametop = document.createElement('p');
+    // var countmid = document.createElement('p');  // TBD
+    var image = document.createElement('img');
+
+
+    // ... and when we click, console.log the value of `name` and `src`
+    elem.addEventListener('click', (function(nameCopy, srcCopy) {
+        return function() {
+            console.log(`Name: ${nameCopy} Path: ${srcCopy}`);
+            image.src = srcCopy;
+            nametop.textContent = nameCopy;
+            // build the display area
+            display.innerHTML = '';
+            display.appendChild(nametop);
+            display.appendChild(image);
+
+        };
+    })(name, src));
+
+    list.appendChild(elem);
+
+};
 
 
 
-//for(var i = 0; i < pictures.length; i++) {
-//    var current_picture = pictures[i];
-//    console.log(i + pictures[i]);
-//    listing.textContent = current_picture;
-//    console.log(generatedFeedHTML);
-//    feed_list.appendChild(listing);
-//}
+
+
+function loadFeed(feedId, feedName, feedCount) {
+    console.log(`Loaded feed ${feedId} ${feedCount} ${feedName}.`);
+
+    document.querySelector('.name').textContent = feedName;
+    document.querySelector('.count').textContent = feedCount;
+    document.querySelector('.display').innerHTML = `<p id="${feedName}" class="name">${feedName}</p>
+                                                 <p id="${feedCount}" class="count">${feedCount}</p>
+                                                 <img src="${pictures[feedId].src}" />`;
+}
